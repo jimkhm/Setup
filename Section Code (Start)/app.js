bef0e46fa9d07fad3113efc82975edcd3454f442
Jim Kim <magnking@gmail.com>
2020-06-03 08:30:00 +0900

    vue-cli commit

--- Section Code (Start)/app.js
+++ Section Code (Start)/app.js
@@ -1,5 +1,5 @@
 new Vue({
-  el: '#app',
+  el: '#app1',
   data: {
     title: 'The VueJS Instance',
     showParagraph: false
@@ -23,4 +23,31 @@ new Vue({
       alert('Title changed, new value: ' + value);
     }
   }
+});
+
+new Vue({
+    el: '#app2',
+    data: {
+        title: 'The second Instance',
+        showParagraph: false
+    },
+    methods: {
+        show: function() {
+            this.showParagraph = true;
+            this.updateTitle('The VueJS Instance (Updated)');
+        },
+        updateTitle: function(title) {
+            this.title = title;
+        }
+    },
+    computed: {
+        lowercaseTitle: function() {
+            return this.title.toLowerCase();
+        }
+    },
+    watch: {
+        title: function(value) {
+            alert('Title changed, new value: ' + value);
+        }
+    }
 });
